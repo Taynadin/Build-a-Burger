2c317221925c4aab2bef35d2bfe6cd364864a322
Nadine Taylor <miami.baby2010@gmail.com>
2018-06-23 19:30:47 -0400

    final commit

---
+++ server.js
@@ -0,0 +1,31 @@
+/* Set up the server file to require npm packages, express middleware, body-parser, methodOverride, espress-handlebars and the handlebars engine
+*/
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var app = express();
+var router = require('./controllers/burgers_controller.js');
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+//setup handlebars templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use('/', router);
+
+// Set up the server
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+console.log("Listening at http://localhost:" + PORT);
+});
+
